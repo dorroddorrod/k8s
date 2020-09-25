0257a3a2bbfd85132b2c13cbb482ed25fb1f1cfd
Dor Mull-or <dor.mullor@appsflyer.com>
2020-09-26 02:55:23 +0300

    Add istio

--- start.sh
+++ start.sh
@@ -7,8 +7,8 @@ kubectl apply -n traefik -f $PWD/infra/networking/ingress-controller/traefik
 sleep 120
 kubectl apply -n cd -f $PWD/infra/continuous-delivery/argo-cd
 sleep 120
-#kubectl apply -f $PWD/infra/faas/openFaas/application.yaml
-#sleep 120
+kubectl apply -f $PWD/infra/faas/openFaas/application.yaml
+sleep 120
 kubectl apply -f $PWD/infra/visibility/prometheus-operator/application.yaml
 sleep 120
 kubectl apply -f $PWD/infra/visibility/metrics-server/application.yaml
@@ -20,11 +20,11 @@ sleep 120
 kubectl apply -f $PWD/infra/networking/service-mesh/istio/application.yaml
 sleep 120
 #kubectl apply -n weave -f $PWD/infra/visibility/weave-scope
-#sleep 10
-#kubectl apply -f $PWD/infra/visibility/fluent-bit/setup/application.yaml
 #sleep 120
-#kubectl apply -f $PWD/infra/visibility/fluent-bit/logging-stack/application.yaml
-#sleep 120
+kubectl apply -f $PWD/infra/visibility/fluent-bit/setup/application.yaml
+sleep 120
+kubectl apply -f $PWD/infra/visibility/fluent-bit/logging-stack/application.yaml
+sleep 120
 kubectl apply -f $PWD/infra/visibility/prometheus-cluster-monitoring/application.yaml
 sleep 120
 kubectl apply -f $PWD/infra/visibility/grafana/application.yaml
@@ -38,12 +38,12 @@ sleep 120
 kubectl apply -f $PWD/infra/visibility/jaeger-operator/application.yaml
 sleep 120
 kubectl apply -f $PWD/infra/visibility/jaeger-cluster/application.yaml
-#sleep 120
-#kubectl apply -f $PWD/applications/example-app-metrics/application.yaml
-#sleep 120
-#kubectl apply -f $PWD/applications/example-app-logs/application.yaml
-#sleep 120
-#kubectl apply -f $PWD/applications/example-app-tracing/application.yaml
+sleep 120
+kubectl apply -f $PWD/applications/example-app-metrics/application.yaml
+sleep 120
+kubectl apply -f $PWD/applications/example-app-logs/application.yaml
+sleep 120
+kubectl apply -f $PWD/applications/example-app-tracing/application.yaml
 sleep 120
 kubectl apply -f $PWD/applications/example-app-istio/application.yaml
 kubectl apply -f service_monitor.yaml
