d73b401dd3f5d77d674792a8def8913de54a50a8
Dor Mullor <dor.mullor@fiverr.com>
2020-11-25 21:58:52 +0200

    Change to helm

--- start.sh
+++ start.sh
@@ -4,14 +4,14 @@ environment=prod
 kubectl apply -f namespaces.yaml
 
 function install {
-  sleep 20
+  sleep 5
   cd $root/$1
   helm dependency update base/
-  helm install $2  -f overlays/$environment/values.yaml -f base/values.yaml  --namespace $3 base/
+  helm upgrade  $2  -f overlays/$environment/values.yaml -f base/values.yaml --install --namespace $3 base/
 }
 install "infra/continuous-delivery/argo-cd" "argo-cd" "cd"
 install "infra/continuous-delivery/argo-rollouts" "argo-rollouts" "cd"
-install "infra/faas/openFaas" "openFaas" "faas"
+install "infra/faas/openFaas" "openfaas" "openfaas"
 #install "infra/networking/ingress-controller" "traefik" "traefik"
 install "infra/networking/service-mesh/istio" "istio-operator" "istio-operator"
 
